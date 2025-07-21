a6658d6d88a04816cbfcb2e45f2d3b7830ec24d2
izu-robin <weavingconstellations@gmail.com>
2025-07-21 17:54:23 +0300

    added UI elements

--- ViewModels/CompleteTestVM.cs
+++ ViewModels/CompleteTestVM.cs
@@ -10,11 +10,77 @@ namespace TestTask2.ViewModels
 {
     class CompleteTestVM : ViewModelBase
     {
+        private bool _isTestInProgress;
+        private bool IsTestInProgress
+        {
+            get
+            {
+                return _isTestInProgress;
+            }
 
+            set
+            {
+                _isTestInProgress = value;
+
+                BtnStatus = value ? "Завершить тест" : "Начать тест";
+                TbStatus = value ? "Текущий тест: " : "Выберите тест: ";
+
+                //OnPropertyChanged();
+            }
+        }
+
+        private bool _isTestFinished;
+        public bool IsTestFinished
+        {
+            get 
+            {
+                return _isTestFinished;
+            }
+            set 
+            {
+                _isTestFinished = value;
+                // и вот тут вот все методы и вообще все, 
+                // что должно произойти по окончанию метода, по сути.
+            }
+        }
+
+
+
+        private string _btnStatus;
+        public string BtnStatus 
+        { 
+            get
+            {
+                return _btnStatus;
+            }
+            set
+            {
+                _btnStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _tbStatus;
+        public string TbStatus
+        {
+            get
+            {
+                return _tbStatus;
+            }
+            set
+            {
+                _tbStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+       
+        
         
         public CompleteTestVM()
         {
-            
+            IsTestInProgress = false;
+           
             
 
 
